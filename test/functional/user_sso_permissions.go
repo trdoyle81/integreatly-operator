e37a555b6fd30f11a3752993ef0d929f545646b9
Triona <trdoyle@redhat.com>
2020-04-06 15:34:55 +0100

    imports for sso permissions

--- test/functional/user_sso_permissions.go
+++ test/functional/user_sso_permissions.go
@@ -2,25 +2,31 @@ package functional
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
+	"common/user_rhmi_developer_permissions"
 
 	"github.com/integr8ly/integreatly-operator/test/resources"
+
+	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	dynclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-const (
-	TestingIDPRealm                = "testing-idp"
-	defaultDedicatedAdminName      = "customer-admin"
-	defaultNumberOfTestUsers       = 2
-	defaultNumberOfDedicatedAdmins = 2
-	defaultSecret                  = "rhmiForeva"
-	DefaultTestUserName            = "test-user"
-	DefaultPassword                = "Password1"
-)
+type TestingContext struct {
+	Client          dynclient.Client
+	KubeConfig      *rest.Config
+	KubeClient      kubernetes.Interface
+	ExtensionClient *clientset.Clientset
+	HttpClient      *http.Client
+	SelfSignedCerts bool
+}
 
 func TestUserSsoPermissions(t *testing.T, ctx *TestingContext) {
 
 	// get console master url
-	rhmi, err := shared_functions.getRHMI(ctx.Client)
+	rhmi, err := user_rhmi_developer_permissions.getRHMI(ctx.Client)
 	if err != nil {
 		t.Fatalf("error getting RHMI CR: %v", err)
 	}
